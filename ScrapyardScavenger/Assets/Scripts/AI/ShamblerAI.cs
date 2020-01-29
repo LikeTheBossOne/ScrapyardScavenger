e44735edb40d9dad7ac93446a59be09c02a6f90d
Christopher DeVoe <cdevoe@bex.net>
2020-01-29 13:51:46 -0500

    Address Issue #12
    Add AI Files

---
+++ ScrapyardScavenger/Assets/Scripts/AI/ShamblerAI.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+//note: enemeyController may be useful to look at
+public class ShamblerAI : MonoBehaviour
+{
+    public Vector3 moveTo;
+    public NavMeshAgent nav;
+    public PlayerManager players;
+    //intent, second based countdown
+    //public int resetDelay = 600;
+    //private int timer;
+    public float aggroTimeLimit;
+    public float wandOffset;
+    public float toPlayerOffset;
+    public float wandAngle;
+    public float wandRad;
+    //public float playerOffset;
+    public ShamblerDetection senses;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //timer = 0;
+        aggroTimeLimit = 10;
+        senses = GetComponent<ShamblerDetection>();
+        nav = GetComponentInParent<NavMeshAgent>();
+        players = FindObjectOfType<PlayerManager>();
+        wandOffset = 10;
+        toPlayerOffset = 20;
+        wandAngle = 60;
+        wandRad = 10;
+        //playerOffset = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //key decision points: player detected, unit shot recently
+        //Time.time-senses.timeShotAt <= aggroTimeLimit ||
+        senses.visionCheck();
+        System.Console.WriteLine(senses.success);
+        if ( senses.detected )
+        {
+            //set target destination to detected/aggressing player or use follow command if there is one
+            //Need to add reorientation/ "lockon camera" for enemy
+            System.Console.WriteLine("Player seen.");
+            transform.LookAt(senses.detected.position, transform.up);
+            setDestination(senses.detected.position);
+        }
+        else
+        {
+            //wander in the direction of closest player
+            //need to rethink this with unity in mind
+            //need to rethink transform.rotate()
+            Vector3 wandDir = (transform.forward-transform.position).normalized;
+            //project the center of the imaginary circle
+            Vector3 center = wandDir * wandOffset;
+            center = center + transform.position;
+            //orient
+            float angle = Random.Range(-1, 1) * wandAngle;
+            //transform.Rotate(transform.up, angle);
+            float curOrient = transform.eulerAngles.y;
+            float newOrient = curOrient + angle;
+            Vector3 dir = transform.forward.normalized;
+            dir.x = Mathf.Sin(newOrient);
+            dir.z = Mathf.Cos(newOrient);
+            //project target spot on circle
+            Vector3 moveTarg = center + wandRad*dir;
+            //correct towards closest player
+            Transform close = findClosestPlayer();
+            
+            Vector3 toPlayer = close.position - moveTarg;
+            toPlayer = toPlayer.normalized;
+            if (distanceToOther(close) < toPlayerOffset)
+            {
+                toPlayer = toPlayer * (float)distanceToOther(close);
+            }
+            else
+            {
+                toPlayer = toPlayer * toPlayerOffset;
+            }
+            
+            moveTarg = moveTarg + toPlayer;
+            //this line is why the blue sphere warps around
+            //close.position = moveTarg;
+            //transform.LookAt(moveTarg, transform.up);
+            moveTo = moveTarg;
+            setDestination(moveTo);
+        }
+    }
+
+    //Finds the closest player
+    Transform findClosestPlayer()
+    {
+        Transform closest = null;
+        double cDist = Mathf.Infinity;
+        //Find closest player or vehicle
+        foreach (Transform ally in players.players)
+        {
+            double dist = distanceToOther(ally.transform);
+            if ( dist < cDist)
+            {
+                closest = ally.transform;
+                cDist = dist;
+            }
+        }
+        return closest;
+    }
+    void setDestination(Vector3 destination)
+    {
+        nav.SetDestination(destination);
+    }
+
+    double distanceToOther(Transform other)
+    {        
+        return Mathf.Sqrt(Mathf.Pow(other.position.x - transform.position.x,2) + Mathf.Pow(other.position.y- transform.position.y,2) + Mathf.Pow(other.position.z-transform.position.z,2));
+    }
+    
+}
---
+++ ScrapyardScavenger/Assets/Scripts/AI/ShamblerDetection.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShamblerDetection : MonoBehaviour
+{
+    public float timeShotAt;
+    //in unity distance units
+    public float visionLimit = 60.0F;
+    public Transform detected;
+    public PlayerManager pManager;
+    public bool success;
+    public bool run;
+    public Collider colliderCheck;
+    public Transform position;
+    public Collider[] hitBox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeShotAt = Mathf.NegativeInfinity;
+        pManager = FindObjectOfType<PlayerManager>();
+        success = false;
+        run = false;
+    }
+    //Handles seeing, capped distance ray cast, currently a detection sphere
+    public bool visionCheck()
+    {
+        //bool success = false;
+        //run = true;
+        position = transform;
+        
+        Collider[] self = GetComponentsInParent<Collider>();
+        hitBox = self;
+        RaycastHit closest = new RaycastHit();
+        closest.distance = Mathf.Infinity;
+        foreach (var p in pManager.players)
+        {
+            if (distance(p) < visionLimit) {
+                RaycastHit[] seen = Physics.RaycastAll(transform.position, p.position-transform.position, visionLimit);
+                foreach (var next in seen)
+                {
+                    if (self.Contains(next.collider) && next.distance < closest.distance)
+                    {
+                        closest = next;
+                    }
+                }
+            }
+        }
+        
+        //front most object is a player
+        //playermanager.contains(closest.collider.getComponentInParent<transform>());
+        //closest.collider.GetComponentInParent<CharacterController>()
+        colliderCheck = closest.collider;
+        if (colliderCheck)
+        {
+            run = true;
+        }
+        if (closest.collider && pManager.players.Contains(closest.collider.GetComponent<Transform>()))
+        {
+            success = true;
+            //detected = the playerObject that hit belongs to
+            detected = closest.collider.GetComponentInParent<Transform>();
+        }
+        System.Console.WriteLine(success);
+        return success;
+    }
+    //Handles being shot, probably an event handler in the future
+    private void gotShot(GameObject shooter)
+    {
+        timeShotAt = Time.time;
+        detected = shooter.transform;
+    }
+    private double distance(Transform other)
+    {
+        return Mathf.Sqrt(Mathf.Pow(transform.position.x - other.position.x, 2) + Mathf.Pow(transform.position.y - other.position.y, 2) + Mathf.Pow(transform.position.z - other.position.z, 2));
+    }
+}
