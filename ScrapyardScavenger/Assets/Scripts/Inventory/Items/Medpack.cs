c61696ec65336284d1251413f1694d2164d22624
Richard Marshall <rtmarsha@ncsu.edu>
2020-02-06 10:26:03 -0500

    Add basic items-medpack and energy drink, not functional yet #47

---
+++ ScrapyardScavenger/Assets/Scripts/Inventory/Items/Medpack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Used by the player to recover health
+ */
+[CreateAssetMenu(fileName = "New Item", menuName = "Resources/Medpack")]
+public class Medpack : Item
+{
+    public Medpack()
+    {
+        // do nothing
+    }
+}
