2fff6ea4fbfac789a2701406c7ad480ea6f3a314
Richard Marshall <rtmarsha@ncsu.edu>
2020-02-07 13:49:28 -0500

    Create scripts for Armors and assets for each

---
+++ ScrapyardScavenger/Assets/Scripts/Inventory/Armors/ChainmailArmor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Equipped by the player to provide a decent boost to
+ * maximum health
+ */
+[CreateAssetMenu(fileName = "New Armor", menuName = "Armors/ChainmailArmor")]
+public class ChainmailArmor : Armor
+{
+    public ChainmailArmor(int id) : base(id, "ChainmailArmor", "Chainmail Armor description here", null)
+    {
+        // do nothing
+    }
+}
