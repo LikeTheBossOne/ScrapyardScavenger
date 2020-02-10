cc18fe719c48bdb9735c4836c74c8878d3988ff6
Christopher DeVoe <cdevoe@bex.net>
2020-02-10 11:18:15 -0500

    Addresses Issue #43
    Add basic prefab for charger and shambler enemies. Add tentative stats to these enemies. Add unfinished ai behavior to charger.

---
+++ ScrapyardScavenger/Assets/Prefabs/Enemies/ChargerStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargerStats : MonoBehaviour
+{
+    public float health { get; private set; }
+    public float damage { get; private set; }
+    public int status { get; private set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 200;
+        damage = 20;
+        status = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onDamage(float damage, GameObject damager, int atkStatus)
+    {
+        health = health - damage;
+        if (atkStatus > 0)
+        {
+            status = atkStatus;
+        }
+    }
+}
---
+++ ScrapyardScavenger/Assets/Prefabs/Enemies/ShamblerStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShamblerStats : MonoBehaviour
+{
+    public float health { get; private set; }
+    public float damage { get; private set; }
+    public float status { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 10;
+        damage = 10;
+        status = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void onDamage(float damage, GameObject damager, int atkStatus)
+    {
+        health = health - damage;
+        if (atkStatus > 0)
+        {
+            status = atkStatus;
+        }
+    }
+}
