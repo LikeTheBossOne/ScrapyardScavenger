1bf628b7cc494fc1629ebc54c6adca22ea9fb053
jstone <jstone@ncsu.edu>
2020-02-14 14:40:15 -0500

    Item pickup sccripting
    Added the Collectable placeholder prefab to the Gauze prefab. Created the ResourcePickup script and added it to the Collectable placeholder prefab.

---
+++ ScrapyardScavenger/Assets/Scripts/Collectable/ResourcePickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcePickup : MonoBehaviour
+{
+    public Transform[] players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = new Transform[2];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If either player is within certain radius of this prefab, destroy it from map and add it to the inventory
+
+    }
+}
