6aa8185f1d8e711b78429c7486cee1a1a9674ed8
jstone <jstone@ncsu.edu>
2020-02-17 13:43:31 -0500

    created ResourcePickup
    Created ResourcePickup script to attach to the Collectable prefab

--- ScrapyardScavenger/Assets/Scripts/Collectable/ResourcePickup.cs
+++ ScrapyardScavenger/Assets/Scripts/Collectable/ResourcePickup.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ResourcePickup : MonoBehaviour
 {
     public Transform[] players;
+    float radius = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         players = new Transform[2];
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If either player is within certain radius of this prefab, destroy it from map and add it to the inventory
-
+        // If either player is within certain radius of this prefab, destroy this from map and add it to the inventory
+        
     }
 }
