7cb27c4a7f6d0d66202083d5944f74704a5030d6
Christopher DeVoe <cdevoe@bex.net>
2020-02-19 14:10:49 -0500

    Projectile Improvements Issue #43
    Projectile objects now appear when shot, but don't move.

--- ScrapyardScavenger/Assets/Scripts/Stats/AcidSpit.cs
+++ ScrapyardScavenger/Assets/Scripts/Stats/AcidSpit.cs
@@ -5,28 +5,31 @@ using Photon.Pun;
 
 public class AcidSpit : MonoBehaviour
 {
-    public GameObject origin { get; set; }
+    public GameObject shooter { get; set; }
     public AIPlayerManager pManage { get; set; }
     public int maxExistTime = 10;
+    public int maxV = 10;
+    public Vector3 direction;
     private void OnEnable()
     {
         pManage = FindObjectOfType<AIPlayerManager>();
         Destroy(gameObject, maxExistTime);
     }
 
-    public void Shoot(GameObject creator)
+    public void Shoot(GameObject creator, Vector3 dir)
     {
-        origin = creator;
+        shooter = creator;
+        direction = dir.normalized;
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (!collision.collider.bounds.Intersects(origin.GetComponent<Collider>().bounds))
+        if (!collision.collider.bounds.Intersects(shooter.GetComponent<Collider>().bounds))
         {
             foreach (Transform player in pManage.players)
             {
                 if (collision.collider.bounds.Contains(player.position))
                 {
-                    player.gameObject.GetPhotonView().RPC("TakeDamage", RpcTarget.All, origin.GetComponent<ShamblerAttacks>().spitDamage);
+                    player.gameObject.GetPhotonView().RPC("TakeDamage", RpcTarget.All, shooter.GetComponent<ShamblerAttacks>().spitDamage);
                 }
             }
             
