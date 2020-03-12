8a38dd4eaa98b475e9d86ffafcfe0e718c8b6b78
Brendan Boss <blboss@ncsu.edu>
2020-03-12 19:22:40 -0400

    Adding a ResourceVolume for spawning resources
    Resources are spawned randomly throughout the map and are able to be picked up. The type of the resource spawned is also random
    The total resource amount is a configurable option in the SpawningVolume's Unity Inspector
    
    In the future I would like to make it so resources of higher value are spawned further away, but value is yet to be determined so I will leave this as a nice-to-have.

---
+++ ScrapyardScavenger/Assets/Scripts/Collectable/ResourceVolume.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Photon.Pun;
+using UnityEngine;
+
+public class ResourceVolume : MonoBehaviourPun
+{
+    public Terrain playArea;
+    public LayerMask ground;
+
+    public GameObject spawnObj;
+    public int spawnAmount;
+
+    private Bounds bounds;
+
+    void Start()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+        SpawnResources();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnResources()
+    {
+        Debug.Log("Spawning");
+        int spawnedCount = 0;
+        bounds = playArea.terrainData.bounds;
+
+        // Spawn the amount of resources
+        while (spawnedCount < spawnAmount)
+        {
+            float randX = Random.Range(playArea.transform.position.x + bounds.min.x, playArea.transform.position.x + bounds.max.x);
+            float randZ = Random.Range(playArea.transform.position.z + bounds.min.z, playArea.transform.position.z + bounds.max.z);
+            Vector3 randStartLoc = new Vector3(randX, 100, randZ);
+
+            Debug.DrawRay(randStartLoc, Vector3.down * 100, Color.red, 10000f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(randStartLoc, Vector3.down, out hit, 100f)
+                && (int) Mathf.Pow(2, hit.collider.gameObject.layer) == ground)
+            {
+                // Randomize
+                GameObject objToSpawn = spawnObj;
+                objToSpawn.GetComponent<ResourcePickup>().type = (ResourceType) Random.Range(0, (int)ResourceType.SIZE);
+
+
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Collectable"), hit.point + new Vector3(0, spawnObj.transform.localScale.y/2, 0), Quaternion.identity);
+                spawnedCount++;
+            }
+        }
+        
+    }
+}
