fc6ad728acb5b16ef6ccdae5e29a693e7834e5d0
Tyler Outlaw <toutlaw@ncsu.edu>
2020-03-23 15:05:25 -0400

    Added Volume slider
    Homebase audio sound can be adjusted with a Volume slider. Also, initial volume has been lowered.

---
+++ ScrapyardScavenger/Assets/Scripts/Home Base/Sound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Sound : MonoBehaviour
+{
+    public AudioSource sound;
+
+    public void SetVolume(float slider)
+    {
+        sound.volume = slider;
+    }
+}
