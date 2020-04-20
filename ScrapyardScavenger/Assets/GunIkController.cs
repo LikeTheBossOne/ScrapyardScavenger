50d066bcb697e4380697edde9a7202f170ae7831
Christopher DeVoe <cdevoe@bex.net>
2020-04-20 16:15:13 -0400

    Start of moving Hands to Guns #81

---
+++ ScrapyardScavenger/Assets/GunIkController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GunIkController : MonoBehaviour
+{
+
+    public bool IkActive;
+    private Transform LeftTarget;
+    private Transform RightTarget;
+    public Transform Guns;
+    private Transform current;
+    protected Animator animator;
+    private int slot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        slot = 0;
+        if (Guns.GetChild(slot))
+        {
+            current = Guns.GetChild(slot);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator)
+        {
+            if (IkActive)
+            {
+
+            }
+        }
+    }
+
+    [PunRPC]
+    void EquipWeapon(int index)
+    {
+        if (index == 0 || index == 1)
+        {
+            slot = index;
+            if (Guns.GetChild(slot))
+            {
+                current = Guns.GetChild(slot);
+            }
+        }
+    }
+}
